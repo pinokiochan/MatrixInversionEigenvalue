3dbcd8052baa8d779a93d69eae8a912b75edd687
pinokiochan <beibarys7ergaliev@gmail.com>
2025-01-03 19:49:16 +0500

    inverse_LU is implemented

--- LU.py
+++ LU.py
@@ -1,4 +1,24 @@
 import numpy as np
+from scipy.linalg import lu, lu_solve, lu_factor
 
 def inverse_using_lu(A):
-    
+    lu_piv = lu_factor(A)
+
+    identity = np.eye(A.shape[0])
+
+    A_inv = np.zeros_like(A, dtype=float)
+    for i in range(A.shape[0]):
+        A_inv[:, i] = lu_solve(lu_piv, identity[:, i])
+
+    return A_inv
+
+A = np.array([[50, 107, 36],
+              [35, 54, 20],
+              [31, 66, 21]], dtype=float)
+
+A_inverse = inverse_using_lu(A)
+
+print("Matrix A:")
+print(A)
+print("\nInverse of Matrix A:")
+print(A_inverse)
